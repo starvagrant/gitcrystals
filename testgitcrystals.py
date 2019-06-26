e4a65ce32a6417edf0f514c447bd2e1e9abf1c97
James Ginns <starvagrant@yahoo.com>
2019-06-25 22:01:25 -0500

    Write function to ensure test repo is in expected state
    Write a function to make sure the repo you're conducting tests
    is it's expected state while running tests.

--- testgitcrystals.py
+++ testgitcrystals.py
@@ -2,8 +2,27 @@
 
 from gitcrystals import GitCrystalsCmd
 import unittest
+import subprocess
 
 current_commit_sha='75f9ce255a19d4f4b347b679e00ebee2ad027046'
+
+def test_clean_repo():
+    command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'rev-list', 'HEAD']
+    process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = process.stdout
+    if not output==current_commit_sha + '\n':
+        print(output)
+        print("Extra commits found in test repository, please reset")
+        return False
+
+    command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'status', '--short']
+    process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = process.stdout
+    if not output=='':
+        print("Working Directory or Index Not Clean. Please clear")
+        return False
+
+    return True
 
 class Tests(unittest.TestCase):
 
@@ -13,8 +32,9 @@ class Tests(unittest.TestCase):
         self.assertTrue(game.do_quit(''))
 
     def test_do_revlist(self):
-        game = GitCrystalsCmd()
-        game.do_revlist('')
-        self.assertEqual(game.output, current_commit_sha + '\n')
+        if test_clean_repo():
+            game = GitCrystalsCmd()
+            game.do_revlist('')
+            self.assertEqual(game.output, current_commit_sha + '\n')
 
 unittest.main()
