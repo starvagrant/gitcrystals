4b52d46109135815e28ddb949311386706d54953
James Ginns <starvagrant@yahoo.com>
2019-06-26 23:18:57 -0500

    Implement do_branch method
    Implement method for allowing user to add branches from CLI

--- gitcrystals.py
+++ gitcrystals.py
@@ -22,6 +22,17 @@ class GitCrystalsCmd(cmd.Cmd):
         self.output = process.stdout
         self.error = process.stderr
 
+    def do_branch(self, args):
+        first_arg = args.split()[0] # do not allow branches with space names
+        command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'branch', first_arg]
+        process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.output = process.stdout
+        self.error = process.stderr
+
+        if len(args.split()) > 1:
+            print('branch names cannot have spaces ' + first_arg + ' used as branch name')
+
+
 if __name__ == '__main__':
     game = GitCrystalsCmd()
     game.cmdloop()
