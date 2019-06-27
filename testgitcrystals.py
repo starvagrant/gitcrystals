bb958d4a129bb72c73558b8e269aa99e94c01078
James Ginns <starvagrant@yahoo.com>
2019-06-27 16:24:27 -0500

    Implement helper functions and constants
    Clean code by implementing the helper functions and constants
    provided by newly created modules.

--- testgitcrystals.py
+++ testgitcrystals.py
@@ -3,28 +3,30 @@
 from gitcrystals import GitCrystalsCmd
 import unittest
 import subprocess
+import gitconstants as G
+import command_wrapper as cw
 
 current_commit_sha='75f9ce255a19d4f4b347b679e00ebee2ad027046'
 current_branch='data'
 
 def test_clean_repo():
-    command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'rev-list', 'HEAD']
-    process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = [G.GIT, G.GIT_DIR, G.WORK_TREE, 'rev-list', 'HEAD']
+    process = cw.run_process(command)
     output = process.stdout
     if not output==current_commit_sha + '\n':
         print(output)
         print("Extra commits found in test repository, please reset")
         return False
 
-    command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'status', '--short']
-    process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = [G.GIT, G.GIT_DIR, G.WORK_TREE, 'status', '--short']
+    process = cw.run_process(command)
     output = process.stdout
     if not output=='':
         print("Working Directory or Index Not Clean. Please clear")
         return False
 
-    command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'show-ref', '--heads']
-    process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = [G.GIT, G.GIT_DIR, G.WORK_TREE, 'show-ref', '--heads']
+    process = cw.run_process(command)
     output = process.stdout
     lines = output.split('\n')
     if not lines[-1:]==['']: # last element will be empty string if only one ref
@@ -52,13 +54,14 @@ class Tests(unittest.TestCase):
             game = GitCrystalsCmd()
             game.do_branch('newbranch')
 
-            command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'branch']
-            process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            command = [G.GIT, G.GIT_DIR, G.WORK_TREE, 'branch']
+            process = cw.run_process(command)
             output = process.stdout
 
-            command = ['git', '--git-dir=game-repo/.git', '--work-tree=./game-repo', 'branch','-d', 'newbranch']
-            process = subprocess.run(command, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            command = [G.GIT, G.GIT_DIR, G.WORK_TREE, 'branch', '-d', 'newbranch']
+            process = cw.run_process(command)
 
             expected = "* data\n  newbranch\n"
             self.assertEqual(output, expected)
+
 unittest.main()
