b09f95d3289b36eb3e5f2c9ed2d0e3cde26905fd
James Ginns <starvagrant@yahoo.com>
2019-06-29 16:31:16 -0500

    Hold repeated values in a class variable
    Obtain a static list of world room names and put the info in a
    mixin class. This reduces the need to create a worldmap object
    solely for getting the key names of the rooms.

--- test_worldmap.py
+++ test_worldmap.py
@@ -3,25 +3,23 @@
 import unittest
 import worldmap
 
+class R():
+    room_names = []
+    world_map = worldmap.WorldMap()
+    for key in world_map.rooms.data.keys():
+        room_names.append(key)
+
 class Tests(unittest.TestCase):
 
     def test_get_north(self):
-        room_names = []
         world_map = worldmap.WorldMap()
-        for key in world_map.rooms.data.keys():
-            room_names.append(key)
-
-        new_room=world_map.get_direction(room_names[1], 'north')
+        new_room=world_map.get_direction(R.room_names[1], 'north')
         expected_room = "Abandoned Treasury"
         self.assertEqual(new_room, expected_room)
 
     def test_get_invalid_north(self):
-        room_names = []
         world_map = worldmap.WorldMap()
-        for key in world_map.rooms.data.keys():
-            room_names.append(key)
-
-        no_room=world_map.get_direction(room_names[0], 'north')
+        no_room=world_map.get_direction(R.room_names[0], 'north')
         self.assertIsNone(no_room)
 
 unittest.main()
