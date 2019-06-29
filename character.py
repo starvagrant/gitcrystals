2cf30a7b262b99b77ffd84b17a36ef758347640e
James Ginns <starvagrant@yahoo.com>
2019-06-29 15:52:15 -0500

    Alter character class structure for easier reading.
    Attempt to decrease number of structures needed to access
    data. This program has conflicting needs: the json files
    it stores should be as readable as possible, and programmer
    access to data should be as readable as possible.
    
    Finding a compromise, make data attributes a dictionary. This
    leads structures like player.data['alive']['alive'] as opposed
    to player.data.alive['alive']['alive'].
    
    Also define boolean structures like player.alive for quick
    access and boolean comparisons.

--- character.py
+++ character.py
@@ -2,31 +2,26 @@
 
 import jsondata
 
-class Data():
-    def __init__(self):
-        self.name = None
-        self.status = None
-        self.alive = None
-        self.location = None
-        self.relationship = None
-
 class Character():
 
     def __init__(self, json_files):
-        self.data = Data()
+        self.data = {}
         for json_file in json_files:
             if json_file.name == "alive":
-                self.data.alive = json_file.data
+                self.data['alive'] = json_file.data
             if json_file.name == "status":
-                self.data.status = json_file.data
+                self.data['status'] = json_file.data
             if json_file.name == "inventory":
-                self.data.alive = json_file.data
+                self.data['inventory'] = json_file.data
             if json_file.name == "location":
-                self.data.alive = json_file.data
+                self.data['location'] = json_file.data
             if json_file.name == "relationship":
-               self.data.relationship = json_file.data
+                self.data['relationship'] = json_file.data
 
-        if not self.data.relationship:
+        if not 'relationship' in self.data:
             self.is_player = True
         else:
             self.is_player = False
+
+        self.alive = self.data['alive']['alive']
+        self.location = self.data['location']['location']
