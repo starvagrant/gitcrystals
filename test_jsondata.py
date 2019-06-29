1011e8fac296a979d9d7491a8c30ae19dd951e75
James Ginns <starvagrant@yahoo.com>
2019-06-28 23:20:05 -0500

    Create wrapper class for json data
    Create a class that can store, read, and write game data in a
    JSON format.

---
+++ test_jsondata.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import jsondata
+import unittest
+from collections import OrderedDict
+
+class Tests(unittest.TestCase):
+
+    def test_jsondata_load(self):
+        inventory = jsondata.JsonData('game-repo','inventory')
+        expected_dict = OrderedDict(weapons=["Unarmed"],gems=["Git Gem"],items=["Distress Note"],armor=["Basic Clothes"])
+        self.assertDictEqual(inventory.data, expected_dict)
+
+    def test_jsondata_write(self):
+        alive = jsondata.JsonData('game-repo','alive')
+        alive.data['alive'] = False
+        alive.write()
+
+        f = open('game-repo/alive.json', 'r')
+        text = f.read()
+        f.close()
+
+        alive.data['alive'] = True
+        alive.write()
+
+        self.assertEqual(text, '{\n    "alive":false\n}\n\n')
+
+unittest.main()
