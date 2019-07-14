985c383ee67717c28d529db7946b5c9884f7868a
James Ginns <starvagrant@yahoo.com>
2019-07-14 12:23:53 -0500

    Add gitglobal functionality to tests
    Remove class specific helper data/functions and implement
    global ones.

--- test/test_gitcrystals.py
+++ test/test_gitcrystals.py
@@ -9,13 +9,6 @@ import unittest
 import subprocess
 from collections import OrderedDict
 
-repodir="game-repo"
-
-def change_location_file(new_location):
-    json_file = JsonData(repodir,"location")
-    json_file.data['location'] = new_location
-    json_file.write()
-
 class Tests(unittest.TestCase):
 
     # Test game quits correctly
@@ -41,30 +34,30 @@ To your west is...
         self.assertEqual(game.display_location(), expected)
 
     def test_display_ground(self):
-        change_location_file("Git Crystal")
+        G.change_location_file("Git Crystal")
 
         game = GitCrystalsCmd()
         expected = "In Git Crystal you see...\n    Git Status Tutorial\n    Intro Git Tutorial\n"
         self.assertEqual(game.display_ground(), expected)
 
-        change_location_file("Mountain Gate")
+        G.change_location_file("Mountain Gate")
 
     def test_display_characters(self):
-        change_location_file("Mountain Gate")
+        G.change_location_file("Mountain Gate")
 
         game = GitCrystalsCmd()
         expected = 'There is no here but you\n'
         self.assertEqual(game.display_characters(), expected)
 
-        change_location_file("Dragon's Lair")
+        G.change_location_file("Dragon's Lair")
         game = GitCrystalsCmd()
         expected = "In Dragon's Lair you see...\n    princess\n    grandfather\n    dragon\n"
         self.assertEqual(game.display_characters(), expected)
 
-        change_location_file("Mountain Gate")
+        G.change_location_file("Mountain Gate")
 
     def test_create_character(self):
-        change_location_file("Mountain Gate")
+        G.change_location_file("Mountain Gate")
         game = GitCrystalsCmd()
         player = game.player
         self.assertEqual(player.js_location.data['location'], "Mountain Gate")
@@ -72,21 +65,21 @@ To your west is...
         self.assertEqual(princess.js_location.data['location'], "Dragon's Lair")
 
     def test_go(self):
-        change_location_file("Mountain Gate")
+        G.change_location_file("Mountain Gate")
         game = GitCrystalsCmd()
         game.do_go('north')
         expected_location = "Git Crystal"
         player_location = game.player.location
-        json_file = JsonData(repodir,"location")
+        json_file = JsonData(G.repodir,"location")
         file_location = json_file.data['location']
 
         self.assertEqual(player_location, expected_location)
         self.assertEqual(file_location, expected_location)
 
-        change_location_file("Mountain Gate")
+        G.change_location_file("Mountain Gate")
 
     def test_do_look(self):
-        change_location_file("Mountain Gate")
+        G.change_location_file("Mountain Gate")
         game = GitCrystalsCmd()
         expected = """You are in Mountain Gate
 To your north is... Git Crystal
